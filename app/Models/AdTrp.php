7969cdc0eb7b5600e62e6ca387dc5fe22c100d74
shasabbir <shasabbir234@gmail.com>
2022-10-22 01:11:49 +0600

    adtrp model edit

--- app/Models/AdTrp.php
+++ app/Models/AdTrp.php
@@ -10,5 +10,5 @@ class AdTrp extends Model
     use HasFactory;
     protected $table="adtrps";
     public $timestamps = false;
-    protected $fillable = ['commercial_name', 'program','channel_id','date','start','finish','timewatched','duration','tvrp','tvr0','reach0','reachp'];
+    protected $fillable = ['commercial_name', 'program','channel_id','date','start','finish','timewatched','duration','tvrp','tvr0','reach0','reachp','playlog_id'];
 }

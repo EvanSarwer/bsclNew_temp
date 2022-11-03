8617fba250a34e1ac2dbf9c9ea53d2fb68240948
shasabbir <shasabbir234@gmail.com>
2022-11-03 20:55:48 +0600

    type filter added in trend general

--- routes/api.php
+++ routes/api.php
@@ -152,10 +152,10 @@ Route::post('/livechannel/activechannellistgraph',[LiveChannelController::class,
 
 //////////Channels////////////
 Route::get('trend/channels', [ChannelController::class, 'trendchannel']);//->middleware('auth.admin.user');
-Route::post('trend/reach/zero', [ChannelController::class, 'reachtrend'])->middleware('auth.admin.user');
+Route::post('trend/reach/zero', [ChannelController::class, 'reachtrend']);//->middleware('auth.admin.user');
 Route::post('trend/reach/percent', [ChannelController::class, 'reachpercenttrend'])->middleware('auth.admin.user');
 Route::post('trend/tvr/percent', [ChannelController::class, 'tvrtrend'])->middleware('auth.admin.user');
-Route::post('trend/tvr/zero', [ChannelController::class, 'tvrtrendzero'])->middleware('auth.admin.user');
+Route::post('trend/tvr/zero', [ChannelController::class, 'tvrtrendzero']);//->middleware('auth.admin.user');
 Route::post('channel/reach/percent', [ChannelController::class, 'reachpercent'])->middleware('auth.admin.user');
 Route::post('channel/definedtrendreachp', [ChannelController::class, 'definedtrendreachp'])->middleware('auth.admin.user');
 Route::post('channel/definedtrendreachp', [ChannelController::class, 'definedtrendreachp'])->middleware('auth.admin.user');

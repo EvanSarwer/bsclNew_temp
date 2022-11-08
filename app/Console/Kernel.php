fcec716ce26a102df9472390c199f6e627822dc7
shasabbir <shasabbir234@gmail.com>
2022-11-08 22:06:09 +0600

    cron time changed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         
         $schedule->command('shutdown:device')->everyMinute();
-        $schedule->command('adtrp:calculate')->dailyAt('03:00');
+        $schedule->command('adtrp:calculate')->dailyAt('00:10');
     }
 
     /**

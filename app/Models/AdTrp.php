8f6a73a1ff1608e1e4eb637fe24ce887ab516776
shasabbir <shasabbir234@gmail.com>
2022-11-20 17:55:18 +0600

    frequency columb adde and program remove in adtrp

--- app/Models/AdTrp.php
+++ app/Models/AdTrp.php
@@ -10,7 +10,7 @@ class AdTrp extends Model
     use HasFactory;
     protected $table="adtrps";
     public $timestamps = false;
-    protected $fillable = ['commercial_name', 'program','channel_id','channel_name','date','start','finish','timewatched','duration','tvrp','tvr0','reach0','reachp','playlog_id'];
+    protected $fillable = ['commercial_name','channel_id','channel_name','date','start','finish','timewatched','duration','tvrp','tvr0','reach0','reachp','c1','c2','c3','c4','c5','c6','c7','c8','c9','c10','playlog_id'];
 
     public function channel(){
         return $this->belongsTo(Channel::class, 'channel_id');

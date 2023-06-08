6d719312feeab677381ab517a51e23c4bbe2556b
Sabbir Hossain <58880363+shasabbir@users.noreply.github.com>
2023-06-08 12:24:12 +0600

    remove future time change

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('remove:future')->dailyAt('00:05');
+        $schedule->command('remove:future')->dailyAt('23:59');
         $schedule->command('shutdown:device')->everyMinute();
         $schedule->command('adtrp:calculate')->dailyAt('00:10');
         $schedule->command('dayparts:generate')->dailyAt('03:00');

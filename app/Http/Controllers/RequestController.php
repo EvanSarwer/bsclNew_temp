5d00602d9d5f37d01b63cec8b248bb963b663093
shasabbir <shasabbir234@gmail.com>
2023-06-17 22:48:53 +0600

    values ignored before 2020

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -119,7 +119,7 @@ class RequestController extends Controller
         $rr->error = $req->error;
         $rr->server_time = Carbon::now()->toDateTimeString();;
         $rr->save();
-        if ((strtotime($req->start) >= strtotime($req->finish))) {
+        if ((strtotime($req->start) >= strtotime($req->finish))||(strtotime($req->start) < strtotime("2020-01-01 00:00:00"))||(strtotime($req->finish) < strtotime("2020-01-01 00:00:00"))) {
             return;
         }
 

9b5b1916dccff9d9e18988baa0834072d64d36a7
EvanSarwer <evansarwer1@gmail.com>
2023-08-27 19:37:49 +0600

    scheduler update some methode remove

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,16 +26,16 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         $schedule->command('remove:future')->dailyAt('00:05')->timezone('Asia/Dhaka');
-        $schedule->command('shutdown:device')->everyMinute();
+        // $schedule->command('shutdown:device')->everyMinute();
         $schedule->command('adtrp:calculate')->dailyAt('00:10')->timezone('Asia/Dhaka');
         $schedule->command('dayparts:generate')->dailyAt('03:00')->timezone('Asia/Dhaka');
         $schedule->command('dayparts:generate')->dailyAt('05:00')->timezone('Asia/Dhaka');
         $schedule->command('dayparts:generate')->dailyAt('07:00')->timezone('Asia/Dhaka');
         $schedule->command('dayparts:generate')->dailyAt('08:00')->timezone('Asia/Dhaka');
         $schedule->command('dashboardGraph:generate')->dailyAt('11:56');
-        $schedule->command('notification:generate')->hourly();
-        $schedule->command('notificationfu:generate')->everySixHours();
-        $schedule->command('notification2hr:generate')->everySixHours();
+        // $schedule->command('notification:generate')->hourly();
+        // $schedule->command('notificationfu:generate')->everySixHours();
+        // $schedule->command('notification2hr:generate')->everySixHours();
         
     }
 

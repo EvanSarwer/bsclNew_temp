62b27d6bee7d6d7c62bdca8dca77d2f8c3eafe1b
EvanSarwer <evansarwer1@gmail.com>
2023-12-21 22:49:57 +0600

    user last respose in last_reqiest time

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Login;
 use App\Models\Token;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class AuthAdmin
 {
@@ -20,6 +22,7 @@ class AuthAdmin
         $token = $request->header('Authorization');
         $userToken = Token::where('token', $token)->first();
         if ($userToken && ($userToken->login->active == 1) && ($userToken->login->role == 'admin'||$userToken->login->role == 'operator')) {
+            Login::where('id', $userToken->login->id)->update(['last_request' => date('Y-m-d H:i:s')]);   // Update app user's Last Request Time
             return $next($request);
         }
         return response()->json(["msg" => "Unauthorized"], 401);
